1c5864ef32267b4d799066583431c9f7c61a98b2
amal <amalmaghraoui405@gmail.com>
2024-06-22 00:08:12 +0100

    Resolved merge conflicts

--- front/src/app/devices/devices.component.ts
+++ front/src/app/devices/devices.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AdddevicesComponent } from '../adddevices/adddevices.component';
 
 @Component({
   selector: 'app-devices',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./devices.component.css']
 })
 export class DevicesComponent {
+  constructor(private dialog: MatDialog) {}
+  openAddUserDialog() {
+    this.dialog.open(AdddevicesComponent, {
+      width: '600px',
+    });
 
+}
 }
